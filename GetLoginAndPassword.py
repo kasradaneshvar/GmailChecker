20eb664b400afaf987350c45108130f9b8dbc687
Nicolas LLOBERA <nllobera@gmail.com>
2013-02-17 17:51:16 +0100

    GMail -> Gmail

--- GetLoginAndPassword.py
+++ GetLoginAndPassword.py
@@ -6,7 +6,7 @@ keyring = GnomeKeyring.get_default_keyring_sync() # login
 
 try:
     matches = GnomeKeyring.find_items_sync(
-        GnomeKeyring.ITEM_GENERIC_SECRET, {"application" : "GMail Checker"});
+        GnomeKeyring.ITEM_GENERIC_SECRET, {"application" : "Gmail Checker"});
     for match in matches:
         print("%s %s" % (match.attributes["account"], match.secret))
 except:
--- SetLoginAndPassword.py
+++ SetLoginAndPassword.py
@@ -9,7 +9,7 @@ password = raw_input('Enter password > ')
 keyring = GnomeKeyring.get_default_keyring_sync() # login
 
 GnomeKeyring.item_create_sync(
-    keyring, GnomeKeyring.ITEM_GENERIC_SECRET, "GMail Checker", 
-    {"account" : username, "application" : "GMail Checker"}, password, True)
+    keyring, GnomeKeyring.ITEM_GENERIC_SECRET, "Gmail Checker", 
+    {"account" : username, "application" : "Gmail Checker"}, password, True)
 
 print("Login and password stored in Gnome Keyring!")
