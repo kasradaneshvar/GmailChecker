ed1914cea90f794510d24deb78454c45f85b7b82
Nicolas LLOBERA <nllobera@gmail.com>
2013-02-17 17:40:00 +0100

    saisie à la console des logins et mots de passe

--- SetLoginAndPassword.py
+++ SetLoginAndPassword.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-#### Settings ####
-username="name@gmail.com"
-password="pass1234"
-##################
-
 import gnomekeyring as GnomeKeyring
+
+username = raw_input('Enter Gmail login (name@gmail.com) > ')
+password = raw_input('Enter password > ')
+
 
 keyring = GnomeKeyring.get_default_keyring_sync() # login
 
 GnomeKeyring.item_create_sync(
     keyring, GnomeKeyring.ITEM_GENERIC_SECRET, "GMail Checker", 
     {"account" : username, "application" : "GMail Checker"}, password, True)
+
+print("Login and password stored in Gnome Keyring!")
