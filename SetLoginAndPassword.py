cad8d6c2a23fa253469810fc5e58106b3e759653
Nicolas LLOBERA <nllobera@gmail.com>
2013-02-17 22:50:18 +0100

    suppression des anciennes entrées avant d'ajouter la nouvelle

--- SetLoginAndPassword.py
+++ SetLoginAndPassword.py
@@ -8,6 +8,16 @@ password = raw_input('Enter password > ')
 
 keyring = GnomeKeyring.get_default_keyring_sync() # login
 
+# delete previous Gmail Checker entries
+try:
+    matches = GnomeKeyring.find_items_sync(
+        GnomeKeyring.ITEM_GENERIC_SECRET, {"application" : "Gmail Checker"});
+    for match in matches:
+        GnomeKeyring.item_delete_sync(keyring, match.item_id)
+except GnomeKeyring.NoMatchError:
+    pass
+
+
 GnomeKeyring.item_create_sync(
     keyring, GnomeKeyring.ITEM_GENERIC_SECRET, "Gmail Checker", 
     {"account" : username, "application" : "Gmail Checker"}, password, True)
