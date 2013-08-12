040c69ce07b31ec86198889e50ea3488eb5d115c
Nicolas LLOBERA <nllobera@gmail.com>
2013-08-12 13:26:12 +0200

    update for a better code

--- popupImageLeftMenuItem.js
+++ popupImageLeftMenuItem.js
@@ -1,7 +1,7 @@
 // Popup Image Left Menu Item for Cinnamon Applet
 // A Popup Menu Item with an icon to the left, the text to the right and a command associated to the menu
 // Developed by Nicolas LLOBERA <nllobera@gmail.com>
-// version: 1.0 (15-02-2013)
+// version: 1.1 (11-06-2013)
 // License: GPLv3
 // Copyright © 2013 Nicolas LLOBERA
 
@@ -10,7 +10,7 @@ const PopupMenu = imports.ui.popupMenu;
 const Gio = imports.gi.Gio;
 const St = imports.gi.St;
 
-/* Redefine a PopupImageMenuItem to get a colored image to the left side */
+// Redefine a PopupImageMenuItem to get a colored image to the left side
 function PopupImageLeftMenuItem() {
     this._init.apply(this, arguments);
 }
@@ -31,19 +31,15 @@ PopupImageLeftMenuItem.prototype = {
         this.addActor(this.label);
     },
     
-    _createIcon: function(iconName)
-    {
+    _createIcon: function(iconName) {
         // if the iconName is a path to an icon
-        if (iconName.indexOf("/") !== -1)
-        {
+        if (iconName[0] === '/') {
             var file = Gio.file_new_for_path(iconName);
             var iconFile = new Gio.FileIcon({ file: file });
             
             return new St.Icon({ gicon: iconFile, icon_size: 24 });
         }
         else // use a themed icon
-        {
             return new St.Icon({ icon_name: iconName, icon_size: 24, icon_type: St.IconType.FULLCOLOR });
-        }
     }
 };
